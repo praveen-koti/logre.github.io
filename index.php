96324e27003b67959bb8a84b8b9498d3b3bbd18a
praveen-koti <51700206+praveen-koti@users.noreply.github.com>
2021-07-16 17:18:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,40 @@
+<!DOCTYPE html>
 <html>
-<head></head>
+<head>
+	<title>login page</title>
+	<link rel="stylesheet" type="text/css" href="test.css">
+	<meta charset="UTF-8">
+</head>
 <body>
-<p>hello everyone</p>
+	<div class="loginbox">
+		<img src="avtar.png" class="avtar">
+		<h1>Login Here</h1>
+		<form action="validation.php" method="post">
+			<p>Username</p>
+			<input type="text" name="Username" placeholder="Enter Username">
+			<p>password</p>
+			<input type="password" name="password" placeholder="Enter Password">
+			<input type="submit" value="Login">
+			<a href="#">Lost your password</a><br>
+			<a href="reg.php">Don't have an account?..REGISTER</a>
+		</form>
+	</div>
+  <script type="text/javascript">
+  	document.addEventListener("mousemove", function(e){
+  		var body = document.querySelector('body');
+  		var heart = document.createElement("span");
+  		var x = e.offsetX;
+  		var y = e.offsetY;
+  		heart.style.left = x+'px';
+  		heart.style.top = y+'px';
+  		var size = Math.random() * 100;
+  		heart.style.width = 20 + size+'px';
+  		heart.style.height = 20 + size+'px';
+  		body.appendChild(heart);
+      setTimeout(function(){
+        heart.remove();
+      },2000)
+  	})
+  </script>
 </body>
 </html>
